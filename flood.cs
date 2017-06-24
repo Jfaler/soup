8db0d8bcb8845da92304711efc9d331aaa301b11
Justin Faler <Jstnfaler@gmail.com>
2017-06-23 19:21:12 -0700

    1.0.1.3
    Fixed missing semicolon on line 18.

--- flood.cs
+++ flood.cs
@@ -15,7 +15,7 @@ namespace CallBomber
     {
 	public static string accountsid = "*********"; 
 	public static string authtoken = "*********";
-	public static List<string> numbers = new List<string>(new string[] { "+10000000", "+10000000", } // Replace "" with phone numbers
+	public static List<string> numbers = new List<string>(new string[] { "+10000000", "+10000000", }; // Replace "" with phone numbers
 	public static List<string> numbersInUse = new List<string>();
 	public static string NumToCall = "";
 	static void Main(string[] args)
